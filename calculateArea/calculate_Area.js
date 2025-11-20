33f36b4fb8d1505337c8d0d3b5d7d9ecbffc8636
alhedai <mohammed.alhedai@gmail.com>
2025-11-20 03:13:10 -0500

    added groceries total

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,6 +1,10 @@
 let length;
 let width;
 
+let grocery2;
+let grocery1;
+let grocery3;
+
 function calculateArea(){
     length = parseFloat(document.getElementById("length").value);
     width = parseFloat(document.getElementById("width").value);
@@ -9,4 +13,14 @@ function calculateArea(){
 
     document.getElementById("result").innerText = `the area of the rec is: ${area}`;
      
+}
+
+function grocerytotal(){
+    grocery1 = parseFloat(document.getElementById("grocery1").value);
+    grocery2 = parseFloat(document.getElementById("grocery2").value);
+    grocery3 = parseFloat(document.getElementById("grocery3").value);
+
+    let total = grocery1 + grocery2 + grocery3;
+
+    document.getElementById("result2").innerText = `total amount for groceries is: ${total}`;
 }
